2dbcc47baf4abec98365954abe8908ae8464d5aa
IvYuCh <goooooooooalie@gmail.com>
2023-08-15 14:35:21 +0300

    add tests; benchmark

---
+++ test/benchmark.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import requests
+from time import perf_counter
+
+
+class Benchmark:
+    def __init__(self, images_dir: str, repeat=100):
+        '''
+        images_dir - str path to dir test images
+        '''
+        self.images = list(Path(images_dir).iterdir())
+        self.images_count = len(self.images)
+        self.url = "http://localhost:18082/multipart/draw_detections"
+        self.repeat = repeat
+
+    def start(self):
+        total_time = []
+        for step in range(self.repeat):
+            step_time = 0
+            print(f"Step {step} of {self.repeat} started")
+            for idx, image in enumerate(self.images):
+                files = {"file": open(image, "rb")}
+                start = perf_counter()
+                requests.post(self.url, files=files)
+                request_time = perf_counter() - start
+                step_time += request_time
+                print(f"Image {idx + 1} of {self.images_count}", end="\r")
+            total_time.append(step_time)
+        mean_time = sum(total_time) / len(total_time)
+        return f"RPS: {self.images_count / mean_time}"
+
+
+# bm = Benchmark()
+# print(bm.start())
---
+++ test/test.py
@@ -0,0 +1,81 @@
+import json
+import time
+import requests
+from benchmark import Benchmark
+import subprocess, shlex
+'''
+Result saved in file benchmarks.json if key not exist
+'''
+
+TYPE_TEST = ['one', 'billion', 'dir']
+DET_MODEL = [
+'scrfd_2.5g_gnkps',
+'yolov5l-face',
+'yolov5m-face',
+'yolov5s-face',
+'yolov5n-face',
+'yolov5n-0.5',
+'scrfd_500m_gnkps',
+'scrfd_10g_gnkps',
+'scrfd_500m_bnkps',
+'scrfd_2.5g_bnkps',
+'scrfd_10g_bnkps',
+'centerface',
+]
+
+REC_MODEL = [
+'w600k_r50',
+'w600k_mbf',
+'glintr100',
+]
+START_SCRIPT_PATH = '/home/ichernoglazov/InsightFace-REST'
+INSIGHT_FACE = 'http://localhost:18082'
+DEBUG_LAG=120
+
+def start_container(det, rec):
+    subprocess.run(args=shlex.split(f'bash ./deploy_trt.sh {det} {rec}'), cwd=START_SCRIPT_PATH)
+    for i in range(10):
+        try:
+            requests.get(f'{INSIGHT_FACE}/info')
+            print('Connected')
+            return True
+        except Exception as e:
+            print(e)
+            print(f'Try connect {i + 1} time. Sleep 5')
+            time.sleep(5)
+    return False
+
+try:
+    result = json.load(open('benchmarks.json', 'rb'))
+except:
+    result = {}
+
+for det in DET_MODEL:
+    for rec in REC_MODEL:
+        
+        if all([f'{det} {rec} {t}' in result for t in TYPE_TEST]) and \
+                any([result[f'{det} {rec} {t}'] == "Fail to start container" 
+                        for t in TYPE_TEST]):
+            continue
+
+        if not start_container(det, rec):
+            for t in TYPE_TEST:
+                if not f'{det} {rec} {t}' in result:
+                    result[f'{det} {rec} {t}'] = 'Fail to start container'
+            json.dump(result, open('benchmarks.json', 'w'), indent=4)
+            continue
+
+        for t in TYPE_TEST:
+            if f'{det} {rec} {t}' in result: 
+                r = result[f'{det} {rec} {t}']
+                if r != 'Fail to start container':
+                    continue
+                print(f'{det} {rec} {t} result: {r}')
+            print(f"Benchmark {det} {rec} {t}")
+            result[f'{det} {rec} {t}'] = Benchmark(images_dir=t).start()
+            json.dump(result, open('benchmarks.json', 'w'), indent=4)
+        
+        time.sleep(DEBUG_LAG)
+
+
+print(json.dumps(result, indent=4))
