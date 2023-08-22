a3ea837fff1960c8c17c83f71b4fa3d9768e7256
IvYuCh <goooooooooalie@gmail.com>
2023-08-22 17:48:07 +0300

    test rec det det+rec mode

--- test/benchmark.py
+++ test/benchmark.py
@@ -1,68 +1,49 @@
 from pathlib import Path
-import requests
 from time import perf_counter
 import glob
 import os
 import sys; sys.path.append('..')
 from demo_client import IFRClient, file2base64, to_chunks
+from itertools import islice, cycle
 
 START_SCRIPT_PATH = '/home/ichernoglazov/InsightFace-REST'
 
 class Benchmark:
-    def __init__(self, images_dir: str, host='http://localhost', port=18081, repeat=100):
+    def __init__(self, images_dir: str, host='http://localhost', port=18081, repeat=100000):
         '''
         images_dir - str path to dir test images
         '''
-        self.images = list(Path(images_dir).iterdir())
         self.images_dir = images_dir
-        self.images_count = len(self.images)
         self.host = host
         self.port = port
         self.draw_url = f"{host}:{port}/multipart/draw_detections"
         self.repeat = repeat
 
-    def start(self):
-        total_time = []
-        for step in range(self.repeat):
-            step_time = 0
-            print(f"Step {step} of {self.repeat} started")
-            for idx, image in enumerate(self.images):
-                files = {"file": open(image, "rb")}
-                start = perf_counter()
-                requests.post(self.draw_url, files=files)
-                request_time = perf_counter() - start
-                step_time += request_time
-                print(f"Image {idx + 1} of {self.images_count}", end="\r")
-            total_time.append(step_time)
-        mean_time = sum(total_time) / len(total_time)
-        return f"RPS: {self.images_count / mean_time}"
-    
-    def start_det(self):
+    def start(self, det_rec_mode, batch_size):
         # start test for detection
         # request api for /extract
+        kwargs = dict(mode='data')
+        if det_rec_mode == 'det':
+            kwargs['extract_embedding'] = False
+        elif det_rec_mode == 'rec':
+            kwargs['embed_only'] = False
+
         client = IFRClient(self.host, self.port)
         files = glob.glob(os.path.join(self.images_dir, '*.*'))
         print('Images will be sent in base64 encoding')
         files = [file2base64(file) for file in files]
-        batches = to_chunks(files, size=1)
+        files = islice(cycle(files), self.repeat)
+        batches = to_chunks(files, size=batch_size)
         batches = [list(chunk) for chunk in batches]
         
         total_time = []
-
-        for step in range(self.repeat):
-            step_time = 0
-            print(f"Step {step} of {self.repeat} started")
-            for idx, batch in enumerate(batches):
-                start = perf_counter()
-                client.extract(data=batch, 
-                            mode='data',
-                                extract_embedding=False)
-                request_time = perf_counter() - start
-                step_time += request_time
-                print(f"Image {idx + 1} of {self.images_count}", end="\r")
-            total_time.append(step_time)
-        mean_time = sum(total_time) / len(total_time)
-        return f"RPS: {self.images_count / mean_time}"
+        for idx, batch in enumerate(batches):
+            print(f"Batch {idx + 1} of {len(batches)} length of batch {len(batch)}", end="\r")
+            start = perf_counter()
+            client.extract(data=batch, **kwargs)
+            request_time = perf_counter() - start
+            total_time.append(request_time)
+        return f"RPS: {self.repeat / sum(total_time)}"
 
 # bm = Benchmark()
 # print(bm.start())
--- test/test.py
+++ test/test.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import sys
 import time
 import requests
 from benchmark import Benchmark
@@ -30,6 +29,12 @@ REC_MODEL = [
 'w600k_mbf',
 'glintr100',
 ]
+MODE = [
+'det+rec', 
+'det',
+'rec'
+]
+    
 START_SCRIPT_PATH = '/home/ichernoglazov/InsightFace-REST'
 DEBUG_LAG=120
 
@@ -58,63 +63,66 @@ def start_container(det, rec, url, cpu=True, gpu=None):
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, choices=[-1, 0, 1], default=-1)
 parser.add_argument('--save-file', type=str, required=True)
-parser.add_argument('--test-types', type=str, nargs='+', choices=TYPE_TEST, default=TYPE_TEST)
+parser.add_argument('--test-types', type=str, nargs='+', choices=TYPE_TEST, default=TYPE_TEST, help='Test types one billion dir for detection, detection + recognition and rec for recognition only')
 parser.add_argument('--det-models', type=str, nargs='+', choices=DET_MODEL, default=DET_MODEL)
 parser.add_argument('--rec-models', type=str, nargs='+', choices=REC_MODEL, default=REC_MODEL)
 parser.add_argument('--port', type=int, required=True)
 parser.add_argument('--host', type=str, default='http://localhost')
-parser.add_argument('--det', action='store_true')
+parser.add_argument('--mode', type=str, choices=MODE)
+parser.add_argument('-b', type=int, choices=[1, 64], default=1)
 
 args = parser.parse_args()
 INSIGHT_FACE = f'{args.host}:{args.port}'
 if args.gpu == -1:
     cpu = True
     gpu = None
-    key_template = '{} {} {} cpu'
+    key_template = '{} {} {} {} {} cpu'
 else:
     cpu = False
     gpu = args.gpu
-    key_template = '{} {} {}'
-if args.det:
-    key_template = f'det {key_template}'
+    key_template = '{} {} {} {} {}'
 
 test_types = set(args.test_types)
 det_models = set(args.det_models)
-# if detection test, choose the lightest rec model 
-rec_models = set(['w600k_mbf'] if args.det else args.rec_models)
 try:
     result = json.load(open(args.save_file, 'rb'))
 except:
     result = {}
+if args.mode == 'det':
+    # if detection test, choose the lightest rec model 
+    rec_models = set(['w600k_mbf'])
+elif args.mode == 'rec':
+    # if detection test, choose the lightest det model 
+    rec_models = set(['w600k_mbf'])
+    det_models = set(['scrfd_500m_bnkps'])
+    test_types = set(['rec'])
 
 for det in det_models:
     for rec in rec_models:
         
-        if all([ key_template.format(det, rec, t) in result for t in test_types]) and \
+        if all([ key_template.format(args.mode, det, rec, t, f'batch={args.b}') in result for t in test_types]) and \
                 all([result[key_template.format(det, rec, t)] != "Fail to start container" 
                         for t in test_types]):
             continue
 
         if not start_container(det, rec, INSIGHT_FACE, cpu, gpu):
             for t in test_types:
-                key = key_template.format(det, rec, t)
+                key = key_template.format(args.mode, det, rec, t, f'batch={args.b}')
                 if not key in result:
                     result[key] = 'Fail to start container'
             json.dump(result, open(args.save_file, 'w'), indent=4)
             continue
 
         for t in test_types:
-            key = key_template.format(det, rec, t)
+            key = key_template.format(args.mode, det, rec, t, f'batch={args.b}')
             if key in result: 
                 r = result[key]
                 if r != 'Fail to start container':
                     continue
                 print(f'{key} result: {r}')
             print(f"Benchmark {key}")
-            if args.det:
-                result[key] = Benchmark(images_dir=t, host=args.host, port=args.port).start_det()
-            else:
-                result[key] = Benchmark(images_dir=t, host=args.host, port=args.port).start()
+            result[key] = Benchmark(images_dir=t, host=args.host, port=args.port).start(det_rec_mode=args.mode, batch_size=args.b)
+
             json.dump(result, open(args.save_file, 'w'), indent=4)
         
         # time.sleep(DEBUG_LAG)
